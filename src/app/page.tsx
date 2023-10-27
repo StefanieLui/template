2add12cba28fe76e792638c597eaa6695c5a5f86
James Pike <jamesandrewpike@gmail.com>
2023-10-26 22:37:23 -0400

    Add Video Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -62,12 +62,15 @@ export default async function Home() {
           </p>
         </FadeIn>
 
-        <Collection/>
+
+
       </Container>
+
+      <CaseStudies caseStudies={caseStudies} />
 
       <Clients />
 
-      <CaseStudies caseStudies={caseStudies} />
+    
 
       <Testimonial
         className="mt-24 sm:mt-32 lg:mt-40"
@@ -121,7 +124,7 @@ function CaseStudies({
 }) {
   return (
     <>
-      <SectionIntro
+      {/* <SectionIntro
         title="Harnessing technology for a brighter future"
         className="mt-24 sm:mt-32 lg:mt-40"
       >
@@ -130,8 +133,45 @@ function CaseStudies({
           challenges. It’s also the cause, so we find ourselves in bit of a
           catch 22 situation.
         </p>
-      </SectionIntro>
+      </SectionIntro> */}
       <Container className="mt-16">
+        <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+          {caseStudies.map((caseStudy) => (
+            <FadeIn key={caseStudy.href} className="flex">
+              <article className="relative flex w-full flex-col rounded-3xl p-6 ring-1 ring-neutral-950/5 transition hover:bg-neutral-50 sm:p-8">
+                <h3>
+                  <Link href={caseStudy.href}>
+                    <span className="absolute inset-0 rounded-3xl" />
+                    <Image
+                      src={caseStudy.logo}
+                      alt={caseStudy.client}
+                      className="h-16 w-16"
+                      unoptimized
+                    />
+                  </Link>
+                </h3>
+                <p className="mt-6 flex gap-x-2 text-sm text-neutral-950">
+                  <time
+                    dateTime={caseStudy.date.split('-')[0]}
+                    className="font-semibold"
+                  >
+                    {caseStudy.date.split('-')[0]}
+                  </time>
+                  <span className="text-neutral-300" aria-hidden="true">
+                    /
+                  </span>
+                  <span>Case study</span>
+                </p>
+                <p className="mt-6 font-display text-2xl font-semibold text-neutral-950">
+                  {caseStudy.title}
+                </p>
+                <p className="mt-4 text-base text-neutral-600">
+                  {caseStudy.description}
+                </p>
+              </article>
+            </FadeIn>
+          ))}
+        </FadeInStagger>
         <FadeInStagger className="grid grid-cols-1 gap-8 lg:grid-cols-3">
           {caseStudies.map((caseStudy) => (
             <FadeIn key={caseStudy.href} className="flex">
