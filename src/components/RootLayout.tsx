84c5fcda31d563eff52a0bdb3526e5c7b7683b1c
James Pike <jamesandrewpike@gmail.com>
2023-10-28 13:00:43 -0400

    Remove border radius

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -257,18 +257,18 @@ function RootLayoutInner({ children }: { children: React.ReactNode }) {
 
       <motion.div
         layout
-        style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
+        style={{ borderTopLeftRadius: 0, borderTopRightRadius: 0 }}
         className="relative flex flex-auto overflow-hidden bg-white pt-14"
       >
         <motion.div
           layout
           className="relative isolate flex w-full flex-col pt-9"
         >
-          <GridPattern
+          {/* <GridPattern
             className="absolute inset-x-0 -top-14 -z-10 h-[1000px] w-full fill-neutral-50 stroke-neutral-950/5 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
             yOffset={-96}
             interactive
-          />
+          /> */}
 
           <main className="w-full flex-auto">{children}</main>
 
